b93a746abc3ca66748962fd74a2813650bb14839
Rene Heinsen <rheinsen@amelco.co.uk>
2019-06-06 16:52:30 +0100

    implementing createOdooConnection function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,38 @@
-export const Greeter = (name: string) => {
-  return `Hello ${name}!`;
-};
+import { IOdooConnection } from "./connections/odooConnection";
+import { OdooConnectionProtocol } from "./connections/odooConnectionProtocol";
+import { OdooJsonRpcConnection } from "./connections/odooJsonRpcConnection";
+import { OdooXmlRpcConnection } from "./connections/odooXmlRpcConnection";
+
+export function createOdooConnection(
+  hostname: string,
+  port: number,
+  db: string,
+  username: string,
+  password: string,
+  protocol: OdooConnectionProtocol = OdooConnectionProtocol.JSON_RPC,
+  secure: boolean = true
+): IOdooConnection | null {
+  if (protocol === OdooConnectionProtocol.JSON_RPC) {
+    return new OdooJsonRpcConnection(
+      hostname,
+      port,
+      db,
+      username,
+      password,
+      secure
+    );
+  }
+
+  if (protocol === OdooConnectionProtocol.XML_RPC) {
+    return new OdooXmlRpcConnection(
+      hostname,
+      port,
+      db,
+      username,
+      password,
+      secure
+    );
+  }
+
+  return null;
+}
