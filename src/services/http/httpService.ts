60b76bb5212dac5a88f302e42fdf282096a097b1
Rene Heinsen <rheinsen@amelco.co.uk>
2019-07-09 15:41:19 +0100

    post return any

--- src/services/http/httpService.ts
+++ src/services/http/httpService.ts
@@ -2,5 +2,5 @@ export interface IHeaders {
   [key: string]: string;
 }
 export interface IHttpService {
-  post: (url: string, data: any, headers: IHeaders) => Promise<string>;
+  post: (url: string, data: any, headers: IHeaders) => Promise<any>;
 }
